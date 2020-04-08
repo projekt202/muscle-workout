f5de5d875ff4ee4b66471894026f1eeb9e4115c5
Luna Davis <luna.davis@LunaDavsMacbook.attlocal.net>
2020-04-07 21:07:05 -0500

    Updates

---
+++ precache-manifest.57b4d361df7d64b532186e1f472949be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e575d3128a03339527319001d161e12f",
+    "url": "/muscle-workout/index.html"
+  },
+  {
+    "revision": "47c562e57f332e24ffb2",
+    "url": "/muscle-workout/static/css/main.62ea6ec5.chunk.css"
+  },
+  {
+    "revision": "c2f41e52bf4b4334157c",
+    "url": "/muscle-workout/static/js/2.b2fb8894.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/muscle-workout/static/js/2.b2fb8894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c562e57f332e24ffb2",
+    "url": "/muscle-workout/static/js/main.ca5941b2.chunk.js"
+  },
+  {
+    "revision": "711e3137d631bbd80be4",
+    "url": "/muscle-workout/static/js/runtime-main.775dabf5.js"
+  }
+]);
